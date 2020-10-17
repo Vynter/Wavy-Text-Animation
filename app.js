04021eae5efdefe0cc1d3f903bab447dc17e1bbd
Vynter <amine-cheraitia@hotmail.com>
2020-10-17 18:04:29 +0200

    final product

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var text = document.querySelector(".txt");
+let text = document.querySelector(".txt");
+var text2 = document.querySelector(".txt2");
 const fulltext = text.textContent;
 const tabTxt = fulltext.split("");
+const fulltext2 = text2.textContent;
+const tabTxt2 = fulltext2.split("");
 text.textContent = "";
+text2.textContent = "";
 
 for (let i = 0; i < tabTxt.length; i++) {
   text.innerHTML += `<span style="--i:${i + 1}">` + tabTxt[i] + "</span>";
+  text2.innerHTML += `<span style="--i:${i + 1}">` + tabTxt2[i] + "</span>";
 }
 console.log(text);
 // const text = document.querySelector(".txt");
@@ -16,3 +21,6 @@ console.log(text);
 //     text.innerHTML += "<span>" + splitText[i] + "</span>";
 
 // }
+
+var text2 = document.querySelector("::after");
+console.log(text2);
